1ba594917c843504afc1800c04b97c49e55a954c
stellakokolaki <32705062+stellakokolaki@users.noreply.github.com>
2018-11-07 16:41:50 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -178,7 +178,7 @@ d3.json("assets/data/us.json", function(data) {
             treemap(root
             .sum(function (d) {
                 if (treeSumSortType == "number") {
-                    color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#73c3bf" ,#a1d8c8, #cbe0a7", "#f2db84"]);
+                    color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#73c3bf" ,"#a1d8c8", "#cbe0a7", "#f2db84"]);
                 /// //"#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"
                     return d["Total College"];
                 } else if (treeSumSortType == "percent") {
